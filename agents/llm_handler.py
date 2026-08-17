a8870114a3fc9e5c7f30a9c32f6c4423e0983bcc
VishSeran <vishwa.sovis@gmail.com>
2026-08-17 13:21:58 +0530

    llm handler-get_llm_response updated

--- agents/llm_handler.py
+++ agents/llm_handler.py
@@ -9,34 +9,60 @@ from configurations.logger import get_logger
 
 
 logger = get_logger("llm-handler")
-dotenv.load_dotenv()
-class LLMHandler:
-    
-    def __init__(self):
-        
-        try:
-            self.model_name = GROQ_MODEL
-            
-            API = os.getenv("GROQ_API")
-            
-            if not API:
-                raise ValueError("Groq api key is missing")
 
-            self.configs = {
-                "configurables": {
-                    "thread_id": "conversational_id"
-                }
-            }
-            
+dotenv.load_dotenv()
+
+
+class LLMHandler:
+
+    def __init__(self):
+
+        try:
+
+            self.model_name = GROQ_MODEL
+
+            api_key = os.getenv("GROQ_API")
+
+            if not api_key:
+                raise ValueError(
+                    "Groq API key is missing"
+                )
+
             self.chat_groq = ChatGroq(
                 model=self.model_name,
-                api_key=API,
+                api_key=api_key,
                 max_tokens=5000,
                 verbose=True
             )
+
+            self.checkpointer = InMemorySaver()
+
+            logger.info(
+                "ChatGroq and checkpointer created"
+            )
+
+        except Exception:
+            logger.exception(
+                "Error in LLM handler"
+            )
+            raise
+        
+    
+    async def get_llm_response(self, query):
+        
+        try:
             
-            logger.info("Chat groq is created")
+            if not query:
+                raise ValueError("Query is missing")
+            
+            response = await self.chat_groq.ainvoke(query)
+            logger.info("response is fetched")
+            return response.content
+            
+        except ValueError:
+            logger.exception("Value error in get_llm_response")
+            raise
         
         except Exception:
-            logger.exception("Error in llm handler")
+            logger.exception("Error in get_llm_response")
             raise
