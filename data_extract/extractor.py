bfbddb029c4af3cfd43bfbe70bc0a1e33020fc5a
VishSeran <vishwa.sovis@gmail.com>
2026-08-17 12:02:16 +0530

    data extract-split_restaurant_data_into_list updated

--- data_extract/extractor.py
+++ data_extract/extractor.py
@@ -9,11 +9,11 @@ from configurations.logger import get_logger
 logger = get_logger('extractor')
 
 
-async def load_dataset(url:Optional[str], file_path:Optional[str]):
+async def load_dataset(url:str, file_path:str):
     
     try:
         
-        if not url or not file_path:
+        if not url and not file_path:
             raise ValueError("Error in data loading, At leaset URL or File Path must be provided!!!")
         
         path = Path(file_path).resolve()
@@ -25,13 +25,13 @@ async def load_dataset(url:Optional[str], file_path:Optional[str]):
                 
                 logger.info("File is received")
                 
-                with open(path, "w") as file:
+                with open(path, "wb") as file:
                     file.write(response.content)
                     
                 logger.info("File is created")
                 
         if path.exists():
-            with open(path, "r") as file:
+            with open(path, "r", encoding="utf-8") as file:
                     data = file.read()
                     
                     if not data:
@@ -40,13 +40,34 @@ async def load_dataset(url:Optional[str], file_path:Optional[str]):
             logger.info("Data loading is completed")
             return data
                 
-            
-            
-        
     except ValueError:
-        logger.exception("Value erro in load_dataset")
+        logger.exception("Value error in load_dataset")
         raise
     
     except Exception:
         logger.exception("Error in load_dataset")
         raise
+    
+def split_restaurant_data_into_list(data:str):
+    
+    try:
+        
+        if not data:
+            raise ValueError("Restaurant data is missing")
+        
+        formatted_data = data.split("\n\n")
+        formatted_data = formatted_data[1:]
+        
+        logger.info("Restaurant data is formatted")        
+        return formatted_data
+        
+        
+    except ValueError:
+        logger.exception("Value error in split_restaurant_data_into_list")
+        raise
+    
+    except Exception:
+        logger.exception("Error in split_restaurant_data_into_list")
+        raise
+    
+  
