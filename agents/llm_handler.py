9aa7e9708e0cecb39aeccedeced6598f81cc5582
VishSeran <vishwa.sovis@gmail.com>
2026-08-17 13:15:57 +0530

    llm handler updated

--- agents/llm_handler.py
+++ agents/llm_handler.py
@@ -1,3 +1,5 @@
+import os
+import dotenv
 
 from langchain_groq import ChatGroq
 from langgraph.checkpoint.memory import InMemorySaver
@@ -7,18 +9,34 @@ from configurations.logger import get_logger
 
 
 logger = get_logger("llm-handler")
-
+dotenv.load_dotenv()
 class LLMHandler:
     
     def __init__(self):
         
-        self.model_name = GROQ_MODEL
-        
-        self.configs = {
-            "configurables": {
-                "thread_id": "conversational_id"
+        try:
+            self.model_name = GROQ_MODEL
+            
+            API = os.getenv("GROQ_API")
+            
+            if not API:
+                raise ValueError("Groq api key is missing")
+
+            self.configs = {
+                "configurables": {
+                    "thread_id": "conversational_id"
+                }
             }
-        }
+            
+            self.chat_groq = ChatGroq(
+                model=self.model_name,
+                api_key=API,
+                max_tokens=5000,
+                verbose=True
+            )
+            
+            logger.info("Chat groq is created")
         
-        checkpointer = 
-        self.chat_groq
+        except Exception:
+            logger.exception("Error in llm handler")
+            raise
