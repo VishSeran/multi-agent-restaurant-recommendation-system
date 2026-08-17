951deb3f4892a8e84acd1e74373662fc925e2dbe
VishSeran <vishwa.sovis@gmail.com>
2026-08-17 11:02:05 +0530

    branch init commit

---
+++ configurations/logger.py
@@ -0,0 +1,16 @@
+import logging
+
+
+def get_logger(name:str):
+    
+    try:
+        
+        
+        
+    except ValueError as e:
+        print(f"Value error: {e}")
+        raise
+    
+    except Exception as e:
+        print(f"Error in {name}: {e}")
+        raise
