f8f37af0bf9e520c08e79729d3d6dabd0695778e
VishSeran <vishwa.sovis@gmail.com>
2026-08-21 19:44:51 +0530

    documents handler - RestaurantVectorDB  updated

--- vectore_store/restaurants_db.py
+++ vectore_store/restaurants_db.py
@@ -9,18 +9,29 @@ logger =  get_logger("restaurant-db")
 
 class RestaurantVectorDB:
     
-    def __init__(self):
+    def __init__(self, restaurants_data):
         
         self.vector_store = Chroma(
             collection_name="restaurants_data",
             persist_directory=DB_DIR
         )
         logger.info("Restaurant chroma db initiated")
+        self.restaurants_data = restaurants_data
         
         self.embedding = embedding_handler
         
         
-    def create_restaurant_vector_store():
+    def create_restaurant_vector_store(self):
         
         try:
             
+            if self.vector_store is None:
+                raise RuntimeError("Please initial restaurants chroma db first")
+            
+            self.vector_store.from_documents(
+                documents=self.restaurants_data,
+                embedding=self.embedding.
+            )
+        except Exception:
+            logger.exception("Error in create_restaurant_vector_store")
+            raise
