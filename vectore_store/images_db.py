f955a7bb7261f218d222c14e430296ce2e7e71f5
VishSeran <vishwa.sovis@gmail.com>
2026-08-21 15:14:45 +0530

    documents handler - ImageVectorDB updated

---
+++ vectore_store/images_db.py
@@ -0,0 +1,16 @@
+
+from configurations.logger import get_logger
+
+
+logger = get_logger("Image-vector-db")
+
+class ImageVectorDB:
+    
+    def __init__(self, image_paths, recipe_data):
+        
+        self.image_paths = image_paths
+        self.recipe_data = recipe_data
+        self.embedding = 
+    
+    def __call__(self, ):
+        pass
