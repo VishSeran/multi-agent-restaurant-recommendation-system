f706c01c9a4cb049b3f4e2a596c7aa9c9c7d317b
VishSeran <vishwa.sovis@gmail.com>
2026-08-25 11:58:39 +0530

    restaurant retriever - fuse result updated

--- retriever/restaurant_retriever.py
+++ retriever/restaurant_retriever.py
@@ -12,3 +12,23 @@ class RestaurantRetriever:
         self.text_retriever = text_retriever,
         self.image_retriever = image_retriever,
         self.reranker = reranker
+        
+    def reciprocal_score(rank:int, constant:int = 60):
+        return 1/ (rank+constant)
+    
+    
+    
+    def fuse_result(
+        self,
+        text_results: list[dict],
+        image_results: list[dict],
+        text_weight:float = 0.7,
+        image_weight:float = 0.3
+    ):
+        
+        try:
+            
+            
+        except Exception:
+            logger.exception("Error in fuse result")
+            raise
