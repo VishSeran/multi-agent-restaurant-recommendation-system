35222e80b303a008a8b213ca0a6fdd33cc93eaa0
VishSeran <vishwa.sovis@gmail.com>
2026-08-27 17:47:41 +0530

    food agent-food style analyze tool updated

--- agents/food_agent.py
+++ agents/food_agent.py
@@ -6,11 +6,20 @@ from configurations.logger import get_logger
 logger = get_logger("food-agent")
 
 @tool
-def food_analyze(food_name:str):
+def food_analyze(food_name:str) -> str:
     
     """Analzye a food item, including ingredients, taste, preparation style
     , and general characteristics
     """
     
     return f"Food analysis for {food_name}"
+
+@tool
+def food_style_analyze (food_name:str) -> str:
+    
+    """Identify the cuisine and food style of a dish
+    """
+    
+    return f"food style analysis for {food_name}"
+
 
