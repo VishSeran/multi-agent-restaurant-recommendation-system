4e99ae9f89e33e2673cf2f40a867db33ecc2b8b7
VishSeran <vishwa.sovis@gmail.com>
2026-08-27 18:04:26 +0530

    food agent- init updated

--- agents/food_agent.py
+++ agents/food_agent.py
@@ -1,5 +1,7 @@
 
+from langchain.agents import create_agent
 
+from agent_tools.food_agent_tools import food_analyze, food_style_analyze, nutrition_analze
 from configurations.logger import get_logger
 from llms.llm_handler import LLMHandler
 
@@ -19,6 +21,37 @@ class FoodAgent:
             
             logger.info("Chat LLM initiated")
             
+            self.food_agent = create_agent(
+                model=self.chat_llm,
+                tools=[food_analyze, food_style_analyze, nutrition_analze],
+                system_prompt="""
+                
+                            You are a food specialist agent.
+
+                        Analyze the user's request and decide which available
+                        food tools are required.
+
+                        Available capabilities:
+
+                        1. food_analysis_tool
+                        Use for questions about ingredients, taste,
+                        preparation, dishes, or food characteristics.
+
+                        2. food_style_tool
+                        Use when determining cuisine, food category,
+                        culinary style, or regional style.
+
+                        3. nutrition_analysis_tool
+                        Use for questions about nutrition, calories,
+                        protein, carbohydrates, fats, or dietary values.
+
+                        You may call one or multiple tools depending on
+                        the user's request.
+
+                        Do not call tools that are irrelevant.
+            
+                """
+            )
              
             
         except Exception:
