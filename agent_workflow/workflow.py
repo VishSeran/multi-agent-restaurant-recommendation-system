a146248bc4de6622966f1807f7ba8885cb1d1f42
VishSeran <vishwa.sovis@gmail.com>
2026-08-28 14:37:19 +0530

    multi agent workflow init updated

--- agent_workflow/workflow.py
+++ agent_workflow/workflow.py
@@ -1,7 +1,9 @@
 
 from langgraph.prebuilt import 
 
+from agents.food_agent import FoodAgent
 from agents.profile_agent import ProfileAgent
+from agents.recommendation_agent import RecommendationAgent
 from configurations.logger import get_logger
 
 
@@ -12,6 +14,21 @@ class MultiAgentWorkflow:
     def __init__(self):
         
         self.profile_agent = ProfileAgent()
-        self.food_agent 
+        self.food_agent = FoodAgent()
+        self.recommendation_agent = RecommendationAgent()
+        
+        logger.info("Agents are initialized")
+        
+        self.build_workflow()
+        
+    
+        
+    
+        
+        
+    
+        
+        
+       
         
         
