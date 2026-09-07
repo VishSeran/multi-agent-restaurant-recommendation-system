f80caa21942f277960085fb370a37b0ced236f6b
VishSeran <vishwa.sovis@gmail.com>
2026-09-07 19:14:25 +0530

    mcp client - init updated

--- mcp_client/client.py
+++ mcp_client/client.py
@@ -1,4 +1,7 @@
 from contextlib import AsyncExitStack
+from mcp.client.streamable_http import streamable_http_client
+from mcp.client import ClientSession
+
 from configurations.configs import BASE_DIR
 from configurations.logger import get_logger
 
@@ -12,6 +15,7 @@ class MCPClient:
         try:
             self.agent = ai_agent
             self.root_dir = root_dir
+            self.server_url = server_url
             self.session = None
             self.exit_stack = AsyncExitStack()
             self.connected = False
@@ -25,9 +29,39 @@ class MCPClient:
         
         try:
             
+            if self.connected:
+                raise RuntimeError("Already connected to the server")
+            
+            if not self.session is None:
+                raise RuntimeError("Client session is already running")
+            
+            mcp_url = f"{self.server_url}/mcp"
+            
+            read,write,s_id = await self.exit_stack.enter_async_context(
+                streamable_http_client(mcp_url)
+            ) 
+            
+            logger.info("Read, Write and session id is initiated")
+            
+            self.session = await self.exit_stack.enter_async_context(
+                ClientSession(
+                    read,
+                    write,
+                )
+            )
+            self.session.initialize()
+            
+            self.connected = True
+            logger.info('Client session in initiated')
+         
             
         except Exception:
             logger.exception("Error in mcp server connection")
+            
+            self.session = None
+            self.connected = False
+            self.exit_stack = AsyncExitStack()
+            
             raise
         
     
