b26f5b985b3d427c6fbc7fa61b6fbf7fb15a3449
VishSeran <vishwa.sovis@gmail.com>
2026-09-12 17:15:18 +0530

    main function updated

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+from configurations.logger import get_logger
+
+
+logger = get_logger("main")
+
+async def main():
+    
+    
+    try:
+        
+        
+        
+    except Exception:
+        logger.exception("Error in main")
+        raise
