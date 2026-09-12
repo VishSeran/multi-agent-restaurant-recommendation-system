c028ae6cc7de312cbd2c92e0f1fac93fe2149921
VishSeran <vishwa.sovis@gmail.com>
2026-09-12 14:55:58 +0530

    ai mcp http host app - conversation updated

--- mcp_http_host_app.py
+++ mcp_http_host_app.py
@@ -1,4 +1,5 @@
 
+
 from configurations.logger import get_logger
 from mcp_client.client import MCPClient
 from mcp_server.server import MCPServer
@@ -34,25 +35,30 @@ class MCPHttpHostApp:
     
     async def conversation(self, user_query:str):
         
-        try:
+        print("Type q or quit to exit the conversation")
+            
+        while(True):
             
             if not user_query:
-                raise ValueError("User query is missing")
-            
-            print("Type q or quit to exit the conversation")
-            
-            
-            if user_query.strip().lower() in (('q', 'quit')):
+                print("User query is missing")
+                continue
+        
+            if user_query.strip().lower() in ('q', 'quit'):
                 print("Exiting the convesation...")
                 break
+
+            try:
+                
+                response = await self.mcp_client.get_ai_client_response(user_query)
+                print("\n"+ response)
             
-        except ValueError as e:
-            logger.error("value error in conversation")
-            raise
-        
-        except Exception:
-            logger.exception("Error in conversation")
-            raise
-        
+            except ValueError as e:
+                logger.error("value error in conversation")
+                raise
+            
+            except Exception:
+                logger.exception("Error in conversation")
+                raise
+            
         
         
